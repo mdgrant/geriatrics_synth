a3526efefa239ec100a2747a650b471fb45a9fe6
mdgrant <mgrantmd@gmail.com>
2023-04-09 12:34:54 -0500

    move gt_theme_mg to end

--- code/functions_geri_2022-11-16.R
+++ code/functions_geri_2022-11-16.R
@@ -108,51 +108,6 @@ type_col <- function(data) {
     # str_c(collapse = "")
 }
 
-# gt style file
-gt_theme_mg <- function(data) {
-  data %>%
-    opt_row_striping() |>
-    opt_table_lines(extent = "none") |>
-    tab_options(
-      table.font.color = "black",
-      data_row.padding = px(2),
-      table.font.size = px(13), # ?12
-      column_labels.font.size = px(13),  # ?12
-      table.align = "left",
-      table_body.border.bottom.width = px(1.7),
-      table.border.top.width = px(1.7),
-      table_body.border.bottom.color = "#9A9EA1",
-      table.border.top.color = "#9A9EA1",
-      table_body.border.bottom.style = "solid",
-      table.border.top.style = "solid",
-      column_labels.border.bottom.color = "#9A9EA1",
-      column_labels.border.bottom.style = "solid",
-      column_labels.border.bottom.width = px(1.3),
-      column_labels.font.weight = "bold",
-      column_labels.padding = px(3),
-      heading.align = "left",
-      footnotes.padding = px(0),
-      footnotes.font.size = px(14),
-      source_notes.padding = px(0),
-    ) |>
-    opt_horizontal_padding(scale = 2) |>
-    tab_style(
-      style = list(cell_text(weight = "bold")),
-      locations = cells_row_groups()
-    ) |>
-    opt_footnote_marks(marks = "letters") |>
-    opt_css(
-      css = "
-      #one .gt_footnote_marks {
-      font-style: normal;
-      font-weight: normal;
-      font-size: 75%;
-      vertical-align: 0.4em;
-      }
-    "
-    )
-}
-
 # simple figure and table caption labels (flexibility for html)
 table_n <- 0
 figure_n <- 0
@@ -396,4 +351,58 @@ prim_sec_out_fun <- function(outcome, refids) {
     distinct() |>
     pull()
   paste0(temp, "%")
+}
+
+
+# gt style file
+gt_theme_mg <- function(data) {
+  data %>%
+    opt_row_striping() |>
+    opt_table_lines(extent = "none") |>
+    # opt_table_font(
+    #   font = list(
+    #     google_font(name = "Source Sans Pro")
+    #   )
+    # ) |>
+    # opt_table_font(stack = "Source Sans Pro") |>
+    tab_options(
+      table.font.color = "black",
+      table.font.names = "Source Sans Pro",
+      data_row.padding = px(2),
+      table.font.size = px(13), # ?12
+      column_labels.font.size = px(13),  # ?12
+      table.align = "left",
+      table_body.border.bottom.width = px(1.7),
+      table.border.top.width = px(1.7),
+      table_body.border.bottom.color = "#9A9EA1",
+      table.border.top.color = "#9A9EA1",
+      table_body.border.bottom.style = "solid",
+      table.border.top.style = "solid",
+      column_labels.border.bottom.color = "#9A9EA1",
+      column_labels.border.bottom.style = "solid",
+      column_labels.border.bottom.width = px(1.3),
+      column_labels.font.weight = "bold",
+      column_labels.padding = px(3),
+      heading.align = "left",
+      footnotes.padding = px(0),
+      footnotes.font.size = px(12),
+      source_notes.padding = px(0),
+    ) |>
+    opt_horizontal_padding(scale = 2) |>
+    tab_style(
+      style = list(cell_text(weight = "bold")),
+      locations = cells_row_groups()
+    ) |>
+    opt_footnote_marks(marks = "letters") |>
+    opt_footnote_spec(spec_ref = "^x", spec_ftr = "^x")
+  # opt_css(
+  #   css = "
+  #   #one .gt_footnote_marks {
+  #   font-style: normal;
+  #   font-weight: normal;
+  #   font-size: 75%;
+  #   vertical-align: 0.4em;
+  #   }
+  # "
+  # )
 }
