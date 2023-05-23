af04d2edacdfae2f1e46e66237cc3d2a8ac16736
mdgrant <mgrantmd@gmail.com>
2023-05-23 08:48:50 -0500

    replace pngs with gt

--- code/opioid_mn_med_2023-04-26.R
+++ code/opioid_mn_med_2023-04-26.R
@@ -1,6 +1,7 @@
 # table opioid mean med time
 opioid_dat <- contin_dat |>
   filter(if_any(matches("opioid"), ~ !is.na(.x))) |>
+  # select(refid, study, arm_id, matches("opioid"))
   select(refid, study, arm_id, arm_n, opioid_m1:opioid_iqru1, opioid_time1, opioid_m2:opioid_iqru2, opioid_time2, opioid_m3:opioid_iqru3, opioid_time3, opioid_m_all:opioid_iqru_all, opioid_time_all) |>
 select(refid:arm_n, ends_with("all")) |>
   rename_with(~ str_replace(.x, "_all", "1"))
@@ -13,3 +14,8 @@ opioid_times_dat <- mean_med_table(opioid_dat, "opioid_", 1, 0) |>
   left_join(study_arm_dat |>
     select(refid, study, arm, arm_id), by = c("refid", "arm_id")) |>
   relocate(study:arm, .after = refid)
+
+
+temp <- opioid_dat |>
+  left_join(drugs_dat |> select(refid, arm_id, drug_recode_abbr)) |>
+  relocate(drug_recode_abbr, .after = arm_id)
